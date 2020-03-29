e5a4a0d06f7482eae7905ed80539a1ef57439e85
Vito Paine <vito.paine@presidiohealth.com>
2020-03-29 15:24:52 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-uploads-vitopn"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://qke2t7bfid.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_ebKP1TZk0",
+        APP_CLIENT_ID: "6seo65nmohl59p82qhss19h9n9",
+        IDENTITY_POOL_ID: "us-east-1:5f5e3688-b4e4-4fc1-95df-5659d6b50bda"
+    }
+};
