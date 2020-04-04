f34a5e41de83e6b42e4b5687cc9440b963b1bb87
Vito Paine <vito.paine@presidiohealth.com>
2020-04-04 13:38:57 -0700

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+    STRIPE_KEY: "pk_test_ZYxs6zHaZZMdboH0omDF9n51",
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
         REGION: "us-east-1",
         BUCKET: "notes-app-uploads-vitopn"
